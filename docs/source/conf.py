8e343e4560da4cd77578895b7e9fff6d412cc7b4
Eilene <eileneftf@gmail.com>
2025-03-30 23:12:42 -0400

    forgot to track new conf location

---
+++ docs/source/conf.py
@@ -0,0 +1,7 @@
+import sphinx_theme
+html_theme = 'stanford_theme'
+html_theme_path = [sphinx_theme.get_html_theme_path()]
+
+# Project information
+project = 'VSAs and Cognition'
+master_doc = 'index'
