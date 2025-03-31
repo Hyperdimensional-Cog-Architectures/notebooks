5ab3e47fb9fcd2ab38058ab5e597661b69e3fc15
Eilene <eileneftf@gmail.com>
2025-03-30 23:08:47 -0400

    switched back to sphinx_theme god this is confusing

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
-import sphinx_rtd_theme
+import sphinx_theme
 html_theme = 'stanford_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_theme.get_html_theme_path()]
 
 # Project information
 project = 'VSAs and Cognition'
